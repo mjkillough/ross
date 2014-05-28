478129ff79093bcecd9254c45f8d2a4aabec03ec
Michael Killough <michaeljkillough@gmail.com>
2014-05-28 20:51:28 +0100

    Set SP for the various processor modes.

--- src/boot.s
+++ src/boot.s
@@ -1,8 +1,39 @@
 .section .init
 .globl _start
 _start:
+
+    @ SP for IRQ mode
+    @ 0xD2 = (PSR_IRQ_MODE|PSR_FIQ_DIS|PSR_IRQ_DIS)
+    mov r0, #0xD2
+    msr cpsr_c, r0
+    mov sp, #0x80000
+
+    @ SP for FIQ mode
+    @ 0xD1 = (PSR_FIQ_MODE|PSR_FIQ_DIS|PSR_IRQ_DIS)
+    mov r0, #0xD1
+    msr cpsr_c, r0
+    mov sp, #0x4000
+
+    @ SP for SVC mode
+    @ 0xD3 = (PSR_SVC_MODE|PSR_FIQ_DIS|PSR_IRQ_DIS)
+    mov r0, #0xD3
+    msr cpsr_c, r0
     mov sp, #0x8000
+
     bl install_interrupt_vector
+
+    @ Mask all BCM2835 specific interrupts
+    mov r1, #0xFF
+    ldr r0, =0x2000B000
+    str r1, [r0, #0x21C] ;@ INT_DISABLE_GPU1
+    str r1, [r0, #0x220] ;@ INT_DISABLE_GPU2
+    str r1, [r0, #0x224] ;@ INT_DISABLE_BASIC
+
+    @ Enable IRQ
+    @ 0x53 = (PSR_SVC_MODE|PSR_FIQ_EN|PSR_IRQ_DIS)
+    @; mov r0, #0x53
+    @; msr cpsr_c, r0
+
     bl kernel_main
 
 halt:
